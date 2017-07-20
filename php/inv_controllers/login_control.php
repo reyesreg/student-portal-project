4f34926ca6da3e56ba4c0f656d87348679d83b8b
reyesreg <regine@horangi.com>
2017-07-20 16:08:18 +0800

    Login working with header

---
+++ php/inv_controllers/login_control.php
@@ -0,0 +1,29 @@
+<?php
+  include('../_config.php');
+
+  if (isset($_POST['id'])) {
+
+    session_start();
+
+    $id = mysqli_real_escape_string($con, $_POST['id']);
+    $pass = mysqli_real_escape_string($con, $_POST['pass']);
+
+    $get_user = "SELECT 
+      uID, type
+      FROM users
+      WHERE 
+      studentID = $id
+      AND
+      password = '$pass'";
+    $result_user = $con->query($get_user) or die(mysqli_error($con));
+    while($row_user = mysqli_fetch_array($result_user)) {
+      $_SESSION['uID'] = $row_user['uID'];
+      $_SESSION['uType'] = $row_user['type'];
+    }
+
+    header('location: ../../index.php');
+
+  } else {
+    header('location: ../../index.php');
+  }
+?>
--- views/main/header.php
+++ views/main/header.php
@@ -1,3 +1,39 @@
+<?php
+  session_start();
+  
+  $display_welcome = "
+    <form method='POST' action='./php/inv_controllers/login_control.php'>
+        <span>LOGIN&nbsp;&nbsp;</span>
+        <input type='text' name='id' placeholder='ID Number...'>
+        <input type='password' name='pass' placeholder='Password...'>
+        <button style='background: none; padding: 0; outline: none;'>
+          <i class='material-icons' style='color: white;margin-left: 20px;background: rgba(255, 255, 255, 0.2);border-radius: 50%;padding: 5px;'>arrow_forward</i>
+        </button>
+      </form>
+  ";
+  if(isset($_SESSION['uID'])) {
+    $uID = $_SESSION['uID'];
+
+    $get_user = "SELECT 
+      firstName, lastName, type
+      FROM users
+      WHERE uID = $uID";
+    $result_user = $con->query($get_user) or die(mysqli_error($con));
+    while($row_user = mysqli_fetch_array($result_user)) {
+      $firstName = $row_user['firstName'];
+      $lastName = $row_user['lastName'];
+      $type = $row_user['type'];
+
+      $display_welcome = "
+        <div>
+          Welcome, $firstName $lastName!
+        </div>
+      ";
+    }
+  }
+
+?>
+
 <html>
 <head>
   <link rel="stylesheet" href="./assets/styles/main.css">
@@ -11,14 +47,7 @@
         <img src="./assets/img/DLS-CSB_Seal.png" alt="benilde logo" height="60px"/>
         <span class="logo-title">WE BENILDE</span>
       </div>
-      <form method="POST" action="">
-        <span>LOGIN&nbsp;&nbsp;</span>
-        <input type="text" name="email" placeholder="ID Number...">
-        <input type="password" name="pass" placeholder="Password...">
-        <button style="background: none; padding: 0; outline: none;">
-          <i class="material-icons" style="color: white;margin-left: 20px;background: rgba(255, 255, 255, 0.2);border-radius: 50%;padding: 5px;">arrow_forward</i>
-        </button>
-      </form>
+      <?php echo $display_welcome ?>
     </div>
     <div class="content">
       <div class="col-side">
